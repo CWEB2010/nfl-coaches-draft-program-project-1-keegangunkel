14a308d92b163db97b1457f94ba2661f7e533a5d
Keegan Gunkel <gunkeec@dunwoody.edu>
2020-01-31 12:48:01 -0600

    Created the program

---
+++ project1/Project1_Keegan_Gunkel/Project1_Keegan_Gunkel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project1_Keegan_Gunkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
